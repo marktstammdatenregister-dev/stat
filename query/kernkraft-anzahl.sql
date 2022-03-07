dd542727d8ed60ca3bf267d13181ed28b7b15a4c
curiousleo <curiousleo@users.noreply.github.com>
2022-03-07 21:14:57 +0100

    Remove Kernkraft

--- query/kernkraft-anzahl.sql
+++
@@ -1,8 +0,0 @@
--- Anzahl aller Kernkrafteinheiten, die aktuell in Betrieb sind.
-select
-  count(EinheitMastrNummer)
-from
-  EinheitKernkraft
-where
-  EinheitBetriebsstatus = 35
-  and EinheitSystemstatus = 472
--- query/kernkraft-brutto.sql
+++
@@ -1,8 +0,0 @@
--- Bruttoleistung aller Kernkrafteinheiten, die aktuell in Betrieb sind.
-select
-  sum(Bruttoleistung)
-from
-  EinheitKernkraft
-where
-  EinheitBetriebsstatus = 35
-  and EinheitSystemstatus = 472

a32a8f5d505ce5cc7cfcf16e99c8c7750b61e452
curiousleo <curiousleo@users.noreply.github.com>
2022-03-09 18:05:00 +0100

    Format

--- toot.py
+++ toot.py
@@ -1,15 +1,15 @@
+#!/usr/bin/env python3
+#
+# Usage:
+#     ./toot.py <toot text file> <attachment file>
+
 from mastodon import Mastodon
+import sys
 
 mastodon = Mastodon(
     access_token="token.secret",
     api_base_url="https://botsin.space",
 )
-
-media = mastodon.media_post("../plot.png")
-mastodon.status_post(
-    """Ausbau erneuerbarer Energien von 2022-03-02 bis 2022-03-09:
-- Solar: 61.17 GW (+0.41 GW) registriert, laut Plan sollten es 61.37 GW sein.
-- Wind an Land: 56.14 GW (+0.02 GW) registriert, laut Plan sollten es 56.48 GW sein.
-- Wind auf See: 7.79 GW (+0.00 GW) registriert, laut Plan sollten es 7.88 GW sein.""",
-    media_ids=[media],
-)
+text = open(sys.argv[1]).read()
+media = mastodon.media_post(sys.argv[2], focus=(-1, -1))
+mastodon.status_post(text, media_ids=[media])

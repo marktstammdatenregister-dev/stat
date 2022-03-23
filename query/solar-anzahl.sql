9219c0ca1bb22ef10fd2aa793c8a48a3c9f19c46
Leonhard Markert <curiousleo@users.noreply.github.com>
2022-03-23 12:51:40 +0000

    Remove "Anzahl" (#4)

--- query/solar-anzahl.sql
+++
@@ -1,8 +0,0 @@
--- Anzahl aller Solareinheiten, die aktuell in Betrieb sind.
-select
-  count(EinheitMastrNummer)
-from
-  EinheitSolar
-where
-  EinheitBetriebsstatus = 35
-  and EinheitSystemstatus = 472
--- query/verbrennung-anzahl.sql
+++
@@ -1,8 +0,0 @@
--- Anzahl aller Verbrennungseinheiten, die aktuell in Betrieb sind.
-select
-  count(EinheitMastrNummer)
-from
-  EinheitVerbrennung
-where
-  EinheitBetriebsstatus = 35
-  and EinheitSystemstatus = 472
